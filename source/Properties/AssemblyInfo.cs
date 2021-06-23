d01593cf501e369687e6337b5750d0368d1e12a5
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-06-23 15:44:42 -0500

    Create AssemblyInfo.cs

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("StageLock")]
+[assembly: AssemblyDescription("An addon for Kerbal Space Program that adds a lock/unlock toggle button to flight.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StageLock (SL)")]
+[assembly: AssemblyCopyright("2021 zer0Kerbal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: Guid("21EDFD8A-BAF5-4E21-8352-FD92E01C73B1")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
